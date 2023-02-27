13047ba0e62c43d20148e9be4795630fdaa10b8b
Shmoon <msk2292@naver.com>
2023-02-27 17:52:21 +0900

    #noissue feat Project Setup

---
+++ DevAssignment/src/main/java/com/aiceltech/devassignment/DevAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.aiceltech.devassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevAssignmentApplication.class, args);
+    }
+
+}
---
+++ DevAssignment/src/test/java/com/aiceltech/devassignment/DevAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiceltech.devassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
